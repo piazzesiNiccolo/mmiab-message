d83ecf8add474d61ef70346e62bfa1760a16230d
davide morucci <morucci46@gmail.com>
2021-12-01 11:08:23 +0100

    add function for retrieve received msg by user id, set endpoint

--- mib/dao/message_manager.py
+++ mib/dao/message_manager.py
@@ -1,6 +1,9 @@
 from mib import db
+from sqlalchemy import and_
 from mib.dao.manager import Manager
 from mib.models.message import Message
+from mib.models.recipient import Recipient
+from mib.dao.recipient_manager import RecipientManager
 
 from flask import current_app as app
 
@@ -56,6 +59,54 @@ class MessageManager(Manager):
             .all()
         )
         return mess
+
+    @classmethod
+    def get_received_messages(cls, id):
+        '''
+            Returns the list of sent messages by a specific user.
+            
+            mess = (
+                db.session.query(Message)
+                .filter(Message.id_sender == id, Message.is_sent == True)
+                .all()
+            )
+            return mess
+        '''
+        """
+        Returns the list of received messages by a specific user,
+        filtering out those which contain unsafe words if the user
+        has this option enabled.
+        """
+        mess = (
+            db.session.query(Message)
+            .filter(Message.is_arrived == True)
+            .filter(
+                Message.recipients.any(
+                    and_(Recipient.id_recipient == id, Recipient.read_deleted == False)
+                )
+            )
+        )
+        '''if (
+            db.session.query(User)
+            .filter(User.id == id, User.content_filter == True)
+            .count()
+            > 0
+        ):
+            mess = mess.filter(Message.to_filter == False)
+        mess = mess.join(User, Message.id_sender == User.id).all()
+        opened_dict = {
+            m.Message.id_message: next(
+                (
+                    rcp.has_opened
+                    for rcp in m.Message.recipients
+                    if rcp.id_recipient == id
+                ),
+                True,
+            )
+            for m in mess
+        }'''
+
+        return mess, opened_dict
 
     #TODO add some checks about recipient
     @classmethod
