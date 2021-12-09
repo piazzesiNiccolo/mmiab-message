774bebc714ff011481453a2b01a3bd82fbc8069c
Lorenzo Volpi <lorenzo.volpi@outlook.com>
2021-12-09 13:00:31 +0100

    comments removed

--- mib/dao/message_manager.py
+++ mib/dao/message_manager.py
@@ -34,23 +34,6 @@ class MessageManager(Manager):
     def update_message(cls, message: Message):
         Manager.update(message=message)
 
-    '''
-    @classmethod
-    def id_message_exists(id_message):
-        """
-        Checks that the id passed corresponds to a message in the db and returns it, raising an exception
-        if no message is found
-        """
-        message = (
-            db.session.query(Message).filter(Message.id_message == id_message).first()
-        )
-
-        if message is None:
-            return None
-        else:
-            return message
-    '''
-
     @classmethod
     def retrieve_by_id(cls, id_: int):
         Manager.check_none(id=id_)
@@ -75,14 +58,6 @@ class MessageManager(Manager):
     #TODO add some checks about recipient
     @classmethod
     def user_can_read(cls, user_id: int, message: Message) -> bool:
-        '''
-        recipients = [rcp.id_recipient for rcp in message.recipients]
-        if message.is_arrived == True:
-            if user_id not in recipients and user_id != message.id_sender:
-                return False
-        elif user_id != message.id_sender:
-            return False
-        '''
         recipients = [rcp.id_recipient for rcp in message.recipients]
         if message.is_arrived == True:
             if user_id not in recipients and user_id != message.id_sender:
--- mib/resources/messages.py
+++ mib/resources/messages.py
@@ -311,20 +311,6 @@ def message_list_draft(id_usr: int):
 
     return jsonify(response_object), 200
 
-'''
-def message_timeline_daily_sent(id_usr: int, data: datetime ):
-
-    year, month, day = data.year, data.month, data.day
-    list_of_messages = MM.get_messages_timeline_year_sent(id_usr, year, month, day )
-    messages_dicts = [m.serialize() for m in list_of_messages]
-
-    response_object = {
-        'status': 'success',
-        'messages': messages_dicts,
-    }
-
-    return jsonify(response_object), 200
-'''
 
 def message_list_sent(id_usr: int):
 
